12fd7c5a4cab3a5bc215557e5e6418b5f5e2e31e
Deathbot545 <85855492+Deathbot545@users.noreply.github.com>
2023-09-20 09:53:15 +0530

    Start
    ok

---
+++ Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Infrastructure.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Infrastructure.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser> // Accessibility level should match or be less restrictive than AppDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Your DbSets and configurations go here
+    }
+}
